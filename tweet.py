101d23b6b2b2575d5b2b6bc31c0edb0fe3150e94
psvaghela <98940597+psvaghela@users.noreply.github.com>
2023-05-31 10:52:19 +0530

    Delete tweet.py

--- tweet.py
+++
@@ -1,8 +0,0 @@
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent="geoapiExercises")
-ladd1 = "texarkana"
-print("Location address:",ladd1)
-location = geolocator.geocode(ladd1)
-print("Latitude and Longitude of the said address:")
-print((location.latitude, location.longitude))
